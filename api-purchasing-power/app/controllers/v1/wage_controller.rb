a85c96838e3fd7f0d6d7c6a4d37b3579b5ce5c95
felipenathan <felipe.nathan@totvs.com.br>
2020-12-21 11:34:58 -0300

    Reindent && automating local database creation

--- api-purchasing-power/app/controllers/v1/wage_controller.rb
+++ api-purchasing-power/app/controllers/v1/wage_controller.rb
@@ -1,13 +1,13 @@
 require_relative '../../services/wage_service.rb'
 
 class V1::WageController < ActionController::API
-    
-    def initialize
-      super
-      @wage_service = WageService.new
-    end
-
-    def index
-        render json: @wage_service.get_wages
-    end
+  
+  def initialize
+    super
+    @wage_service = WageService.new
+  end
+  
+  def index
+    render json: @wage_service.get_wages
+  end
 end
--- api-purchasing-power/app/services/request_quandl_service.rb
+++ api-purchasing-power/app/services/request_quandl_service.rb
@@ -2,46 +2,46 @@ require 'net/http'
 require 'json'
 
 class RequestQuandlService
-
-    def initialize
-        @api_base = ENV['QUANDL_API']
+  
+  def initialize
+    @api_base = ENV['QUANDL_API']
+  end
+  
+  def request_bmi( country, limit = 1 )
+    uri = URI("#{@api_base}/ECONOMIST/BIGMAC_#{country}/data.json")    
+    bmi = do_get uri, { :limit => limit }
+    parse_bmi bmi
+  end
+  
+  def request_brazilian_wage( limit = 1 )
+    uri = URI("#{@api_base}/BCB/1619/data.json")
+    bmi = do_get uri, { :limit => limit }
+    parse_bmi bmi
+  end
+  
+  private
+  def parse_bmi(bmi)
+    
+    bmi = JSON.parse(bmi, object_class: OpenStruct)
+    
+    return {
+      :columns => bmi.dataset_data.column_names,
+      :data => bmi.dataset_data.data
+    }
+  end
+  
+  def do_get(uri, params = {})
+    
+    params.merge!({
+      :api_key => ENV['QUANDL_API_KEY']
+      })
+      
+      uri.query = URI.encode_www_form(params)
+      
+      response = Net::HTTP.get_response uri
+      
+      raise "Failed to load data: #{response.message}" unless response.is_a? Net::HTTPSuccess
+      
+      return response.body
     end
-
-    def request_bmi( country, limit = 1 )
-        uri = URI("#{@api_base}/ECONOMIST/BIGMAC_#{country}/data.json")    
-        bmi = do_get uri, { :limit => limit }
-        parse_bmi bmi
-    end
-
-    def request_brazilian_wage( limit = 1 )
-        uri = URI("#{@api_base}/BCB/1619/data.json")
-        bmi = do_get uri, { :limit => limit }
-        parse_bmi bmi
-    end
-
-    private
-        def parse_bmi(bmi)
-
-            bmi = JSON.parse(bmi, object_class: OpenStruct)
-
-            return {
-                :columns => bmi.dataset_data.column_names,
-                :data => bmi.dataset_data.data
-            }
-        end
-
-        def do_get(uri, params = {})
-
-            params.merge!({
-                :api_key => ENV['QUANDL_API_KEY']
-            })
-
-            uri.query = URI.encode_www_form(params)
-
-            response = Net::HTTP.get_response uri
-
-            raise "Failed to load data: #{response.message}" unless response.is_a? Net::HTTPSuccess
-        
-            return response.body
-        end
-end
+  end
